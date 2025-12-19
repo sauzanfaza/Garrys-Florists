7e95d390a8b69c4250338ff24f19a76b46221728
Sauzan Faza <sauzanfaza@gmail.com>
2025-12-19 11:13:44 +0700

    fix object cover and text in hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ export default function Home() {
         <Navbar />
         <section className="relative mx-auto h-[80vh]">
             <img src="./assets/Home-hero.jpg" alt="home-hero" 
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover object-center"
             />
             {/* overlay */}
             <div className="absolute inset-0 bg-black/40"></div>
             {/* text */}
-            <div className="absolute inset-0 md:text-6xl">
-                <h1>Embrace the Beauty of <br />
+            <div className="absolute inset-0 flex items-end px-6 py-8 md:px-20">
+                <h1 className="text-white text-3xl font-bold md:text-4xl leading-tight max-w-3xl">Embrace the Beauty of <br />
                 Nature, Crafted by Gary's <br />
                 Florists in Southampton</h1>
             </div>
